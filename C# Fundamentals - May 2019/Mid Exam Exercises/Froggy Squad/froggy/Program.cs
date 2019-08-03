27bf5d6ab6a27ffc203c9deef97f14d1308c321a
TsvetomirAleksandrov <cecko.aleksandrov@gmail.com>
2019-08-03 11:57:32 +0300

    Mid Exam

---
+++ C# Fundamentals - May 2019/Mid Exam Exercises/Froggy Squad/froggy/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frogNames = Console.ReadLine().Split(" ").ToList();
+            var print = new List<string>();
+            var input = string.Empty;
+            
+            while (!(input = Console.ReadLine()).Contains("Print"))
+            {
+                var tokens = input.Split(" ").ToArray();
+                var command = tokens[0];
+
+
+                if (command == "Join")
+                {
+                    string name = tokens[1];
+                    if (!frogNames.Contains(name))
+                    {
+                        frogNames.Add(name);
+                    }
+                }
+
+                else if (command == "Jump")
+                {
+                    string name = tokens[1];
+                    int index = int.Parse(tokens[2]);
+
+                    if (index >= 0 && index < frogNames.Count)
+                    {
+                        frogNames.Insert(index, name);
+                    }
+                }
+
+                else if (command == "Dive")
+                {
+                    int index = int.Parse(tokens[1]);
+
+                    if (index >= 0 && index < frogNames.Count)
+                    {
+                        frogNames.RemoveAt(index);
+                    }
+                }
+
+                else if (command == "First")
+                {
+                    int count = int.Parse(tokens[1]);
+
+                    if (count >= frogNames.Count)
+                    {
+                        count = frogNames.Count;
+                    }
+                    print = frogNames.Take(count).Select(x => x).ToList();
+                    Console.WriteLine(string.Join(" ", print));
+                }
+
+                else if (command == "Last")
+                {
+                    int count = int.Parse(tokens[1]);
+
+                    if (count >= frogNames.Count)
+                    {
+                        count = frogNames.Count;
+                    }
+                    frogNames.Reverse();
+                    print = frogNames.Take(count).Select(x => x).ToList();
+                    print.Reverse();
+                    frogNames.Reverse();
+                    Console.WriteLine(string.Join(" ", print));
+                }    
+            }
+            if (input.Split(" ")[1] == "Reversed")
+            {
+                frogNames.Reverse();
+            }
+            Console.WriteLine($"Frogs: {string.Join(" ", frogNames)}");
+        }
+    }
+}
---
+++ C# Fundamentals - May 2019/Mid Exam Exercises/Giftbox Coverage/giftboxCoverage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace giftboxCoverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizeOfaSide = double.Parse(Console.ReadLine());
+            int sheets = int.Parse(Console.ReadLine());
+            var singleSheetCovers = double.Parse(Console.ReadLine());
+
+            var area = sizeOfaSide * sizeOfaSide * 6;
+            var numOfSheets = sheets;
+            double coveredArea = 0;
+
+            for (int i = 1; i <= numOfSheets; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    coveredArea += singleSheetCovers * 0.25;
+                    sheets -= 1;
+                }
+                else
+                {
+                    coveredArea += singleSheetCovers;
+                    sheets -= 1;
+                }
+            }
+
+            double percentage = (coveredArea / area) * 100;
+            Console.WriteLine($"You can cover {percentage:f2}% of the box.");
+        }
+    }
+}
