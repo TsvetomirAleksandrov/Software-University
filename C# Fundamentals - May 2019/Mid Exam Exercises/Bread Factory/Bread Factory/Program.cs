80fbe8cecff8a3b76b7160e9111ce66febe7776f
TsvetomirAleksandrov <cecko.aleksandrov@gmail.com>
2019-08-05 19:02:19 +0300

    Mid Exam

---
+++ C# Fundamentals - May 2019/Mid Exam Exercises/Bread Factory/Bread Factory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> bestBatch = new List<int>();
+            string command;
+            int quality = -1000000;
+
+            while ((command = Console.ReadLine()) != "Bake It!")
+            {
+                List<int> currentBatch = command.Split("#").Select(int.Parse).ToList();
+
+
+                if (quality < currentBatch.Sum())
+                {
+                    quality = currentBatch.Sum();
+                    bestBatch = currentBatch;
+                }
+                else if (quality == currentBatch.Sum())
+                {
+                    double averageForCurrentBestBatch = quality / bestBatch.Count;
+                    double averageForThisBatch = currentBatch.Sum() / currentBatch.Count;
+                    if (averageForThisBatch > averageForCurrentBestBatch)
+                    {
+
+                        bestBatch = currentBatch;
+                    }
+                    if (averageForCurrentBestBatch == averageForThisBatch)
+                    {
+                        if (currentBatch.Count < bestBatch.Count)
+                        {
+                            bestBatch = currentBatch;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Best Batch quality: {quality}");
+            Console.WriteLine(string.Join(' ', bestBatch));
+
+        }
+    }
+}
