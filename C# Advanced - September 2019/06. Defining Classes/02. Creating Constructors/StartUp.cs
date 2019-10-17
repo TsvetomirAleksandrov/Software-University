0fdaaea8daadf2275b400fc743119fbd7f1027a0
TsvetomirAleksandrov <cecko.aleksandrov@gmail.com>
2019-10-17 22:03:48 +0300

    Homework

---
+++ C# Advanced - September 2019/06. Defining Classes/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            Person person2 = new Person(2);
+            Person person3 = new Person("Pesho", 30);
+        }
+    }
+}
---
+++ C# Advanced - September 2019/06. Defining Classes/03. Oldest Family Member/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            this.people.Add(person);
+        }
+
+
+        public Person GetOldestMember()
+        => people.OrderByDescending(a => a.Age).FirstOrDefault();
+        
+    }
+}
