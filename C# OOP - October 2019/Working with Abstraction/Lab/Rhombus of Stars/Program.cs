848d46456453beb72d36602061e54f97f215a116
TsvetomirAleksandrov <cecko.aleksandrov@gmail.com>
2019-11-07 21:27:45 +0200

    Homework

---
+++ C# OOP - October 2019/Working with Abstraction/Lab/Rhombus of Stars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            for (int stCount = 1; stCount <= size; stCount++)
+            {
+                PrintRow(size, stCount);
+            }
+            for (int stCount = size - 1; stCount >= 1; stCount--)
+            {
+                PrintRow(size, stCount);
+            }
+        }
+
+        static void PrintRow(int figureSize, int starCount)
+        {
+            for (int i = 0; i < figureSize - starCount; i++)
+            {
+                Console.Write(" ");
+            }
+            for (int col = 1; col < starCount; col++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("*");
+        }
+    }
+}
