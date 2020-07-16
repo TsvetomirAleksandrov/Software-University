315956dfd33ee595906e450894964fce229a7463
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-16 18:37:31 +0300

    My First ASP.NET Core Website

---
+++ Entity Framework Core - June 2020/07. Auto-Mapping-Objects-Project/FastFood.Core/ViewModels/Orders/CreateOrderEmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FastFood.Core.ViewModels.Orders
+{
+    public class CreateOrderEmployeeViewModel
+    {
+   
+        public int EmployeeId { get; set; }
+
+       
+        public string EmployeeName { get; set; }
+    }
+}
