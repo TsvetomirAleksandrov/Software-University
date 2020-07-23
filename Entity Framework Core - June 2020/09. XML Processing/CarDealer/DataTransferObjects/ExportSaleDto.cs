555bdb18d9992a8090c812c8008ccdb7e21d90cc
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-23 15:29:59 +0300

    XML Processing Homework

---
+++ Entity Framework Core - June 2020/09. XML Processing/CarDealer/DataTransferObjects/ExportSaleDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObjects
+{
+    [XmlType("Sale")]
+    public class ExportSaleDto
+    {
+        [XmlElement("car")]
+        public ExportCarDto Car { get; set; }
+
+        [XmlElement("discount")]
+        public decimal Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public decimal PriceWithDiscount { get; set; }
+    }
+
+    [XmlType("car")]
+    public class ExportCarDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("car")]
+        public string Model { get; set; }
+
+        [XmlAttribute("travelled-distance")]
+        public long TraveledDistance { get; set; }
+    }
+}
