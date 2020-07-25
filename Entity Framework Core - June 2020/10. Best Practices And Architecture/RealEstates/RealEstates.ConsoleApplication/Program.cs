785a60dbd858c08043e9ba17e48463176e49959f
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-25 17:43:36 +0300

    Update

--- Entity Framework Core - June 2020/10. Best Practices And Architecture/RealEstates/RealEstates.ConsoleApplication/Program.cs
+++ Entity Framework Core - June 2020/10. Best Practices And Architecture/RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Services;
+using System;
+using System.Text;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -6,7 +10,31 @@ namespace RealEstates.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var db = new RealEstateDbContext();
+            db.Database.Migrate();
+
+            IPropertiesService propertiesService = new PropertiesService(db);
+
+            Console.Write("Min price: ");
+            int minPrice = int.Parse(Console.ReadLine());
+            Console.Write("Max price: ");
+            int maxPrice = int.Parse(Console.ReadLine());
+            var properties = propertiesService.SearchByPrice(minPrice, maxPrice);
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.District}, fl. {property.Floor}, {property.Size} m², {property.Year}, {property.Price}€, {property.PropertyType}, {property.BuildingType}");
+            }
+
+            Console.WriteLine(new string('-', 60));
+
+            IDistrictsService districtsService = new DistrictsService(db);
+            var districts = districtsService.GetTopDistrictsByAveragePrice(100);
+            foreach (var district in districts)
+            {
+                Console.WriteLine($"{district.Name} => Price: {district.AveragePrice:0.00} ({district.MinPrice}-{district.MaxPrice}) => {district.PropertiesCount} properties");
+            }
         }
     }
 }
