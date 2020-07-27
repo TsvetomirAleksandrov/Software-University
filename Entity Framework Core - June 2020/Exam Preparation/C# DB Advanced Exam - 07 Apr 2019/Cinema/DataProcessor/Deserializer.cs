7dce4e6d6f32369e6c228facf20218228863a527
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-27 12:07:30 +0300

    ExamPrep

---
+++ Entity Framework Core - June 2020/Exam Preparation/C# DB Advanced Exam - 07 Apr 2019/Cinema/DataProcessor/Deserializer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Cinema.Data.Models;
+using Cinema.DataProcessor.ImportDto;
+using Newtonsoft.Json;
+using ValidationContext = AutoMapper.ValidationContext;
+
+namespace Cinema.DataProcessor
+{
+    using System;
+
+    using Data;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+        private const string SuccessfulImportMovie 
+            = "Successfully imported {0} with genre {1} and rating {2}!";
+        private const string SuccessfulImportHallSeat 
+            = "Successfully imported {0}({1}) with {2} seats!";
+        private const string SuccessfulImportProjection 
+            = "Successfully imported projection {0} on {1}!";
+        private const string SuccessfulImportCustomerTicket 
+            = "Successfully imported customer {0} {1} with bought tickets: {2}!";
+
+        public static string ImportMovies(CinemaContext context, string jsonString)
+        {
+            var moviesDtos = JsonConvert.DeserializeObject<MovieImportDto[]>(jsonString);
+            var movies = new List<Movie>();
+            var sb = new StringBuilder();
+
+            foreach (var dto in moviesDtos)
+            {
+                if (IsValid(dto))
+                {
+                    var movie = new Movie
+                    {
+                        Title = dto.Title,
+                        Genre = dto.Genre,
+                        Duration = dto.Duration,
+                        Rating = dto.Rating,
+                        Director = dto.Director
+                    };
+
+                    movies.Add(movie);
+                    sb.AppendLine(string.Format(SuccessfulImportMovie, dto.Title, dto.Genre, dto.Rating.ToString("F2")));
+                }
+
+                else
+                {
+                    sb.AppendLine(ErrorMessage);
+                }
+            }
+
+            context.Movies.AddRange(movies);
+            context.SaveChanges();
+            return sb.ToString().Trim();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validator = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            var res = Validator.TryValidateObject(obj, validator, validationResult, true);
+            return res;
+        }
+
+        public static string ImportHallSeats(CinemaContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportProjections(CinemaContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportCustomerTickets(CinemaContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
