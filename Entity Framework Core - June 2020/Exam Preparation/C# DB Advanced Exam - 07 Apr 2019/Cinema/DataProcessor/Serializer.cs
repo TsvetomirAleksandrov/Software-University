7086583e247591906ea78e97a04079af1928b1bc
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-27 14:51:10 +0300

    Update

--- Entity Framework Core - June 2020/Exam Preparation/C# DB Advanced Exam - 07 Apr 2019/Cinema/DataProcessor/Serializer.cs
+++ Entity Framework Core - June 2020/Exam Preparation/C# DB Advanced Exam - 07 Apr 2019/Cinema/DataProcessor/Serializer.cs
@@ -1,4 +1,9 @@
-﻿namespace Cinema.DataProcessor
+﻿using System.Linq;
+using Cinema.DataProcessor.ExportDto;
+using Cinema.DataProcessor.ImportDto;
+using Newtonsoft.Json;
+
+namespace Cinema.DataProcessor
 {
     using System;
 
@@ -8,7 +13,38 @@
     {
         public static string ExportTopMovies(CinemaContext context, int rating)
         {
-            throw new NotImplementedException();
+            var movies = context
+                .Movies
+                .Where(m => m.Rating >= rating && m.Projections.Any(p => p.Tickets.Count > 0))
+                .OrderByDescending(m => m.Rating)
+                .ThenByDescending(m => m.Projections.Sum(p => p.Tickets.Sum(t => t.Price)))
+                .Select(m => new MovieExportDto
+                {
+                    MovieName = m.Title,
+                    Rating = m.Rating.ToString("F2"),
+                    TotalIncomes = m.Projections.Sum(p => p.Tickets.Sum(t => t.Price)).ToString("F2"),
+                    Customers = m.Projections.SelectMany(p => p.Tickets
+                           
+                            .Select(c => new CustomerMovieExportDto
+                            {
+                                FirstName = c.Customer.FirstName,
+                                LastName = c.Customer.LastName,
+                                Balance = c.Customer.Balance.ToString("F2")
+                            })
+                            .OrderByDescending(c => c.Balance)
+                            .ThenBy(c => c.FirstName)
+                            .ThenBy(c => c.LastName)
+                            
+                        )
+                        
+                        .ToList()
+                        
+                })
+                .Take(10)
+                .ToList();
+
+            var res = JsonConvert.SerializeObject(movies, Formatting.Indented);
+            return res;
         }
 
         public static string ExportTopCustomers(CinemaContext context, int age)
