c4be061168ab71a6c362033ad0707aeba29baeb8
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-28 13:11:21 +0300

    RealEstates .NET Completed

--- Entity Framework Core - June 2020/RealEstates/RealEstates.Web/Controllers/HomeController.cs
+++ Entity Framework Core - June 2020/RealEstates/RealEstates.Web/Controllers/HomeController.cs
@@ -5,27 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RealEstates.Services;
 using RealEstates.Web.Models;
 
 namespace RealEstates.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IDistrictsService districtsService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IDistrictsService districtsService)
         {
-            _logger = logger;
+            this.districtsService = districtsService;
         }
 
         public IActionResult Index()
         {
-            return View();
+
+            var districts = this.districtsService.GetTopDistrictsByAveragePrice(1000);
+            return View(districts);
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return this.Redirect("https://google.com");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
