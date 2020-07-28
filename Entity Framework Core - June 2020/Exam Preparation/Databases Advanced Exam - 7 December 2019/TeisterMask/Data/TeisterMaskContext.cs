5633e032a3e8f0cfc8dec6d9b605d35f7f86997d
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-07-28 16:37:48 +0300

    Exam Preparation Update

---
+++ Entity Framework Core - June 2020/Exam Preparation/Databases Advanced Exam - 7 December 2019/TeisterMask/Data/TeisterMaskContext.cs
@@ -0,0 +1,34 @@
+﻿using TeisterMask.Data.Models;
+
+namespace TeisterMask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TeisterMaskContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<EmployeeTask> EmployeesTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeTask>(entity =>
+            {
+                entity.HasKey(x => new { x.TaskId, x.EmployeeId });
+            });
+        }
+    }
+}
