a299d4dc1ac5b733494bca2f5a64b4f3dd2c3748
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-08-02 11:37:14 +0300

    Exam Preparation

---
+++ Entity Framework Core - June 2020/Exam Preparation/Database Advanced Exam - 13 Dec 2019/BookShop/DataProcessor/ImportDto/ImportBookDto.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Book")]
+    public class ImportBookDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Range(1,3)]
+        [XmlElement("Genre")]
+        public int Genre { get; set; }
+
+        [XmlElement("Price")]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        [XmlElement("Pages")]
+        [Range(50, 5000)]
+        public int Pages { get; set; }
+
+        [XmlElement("PublishedOn")]
+        public string PublishedOn { get; set; }
+    }
+}
