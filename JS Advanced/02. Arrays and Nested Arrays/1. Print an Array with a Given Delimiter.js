c53f69b97e9e374dd9b015d2b2c1da72d541a6ca
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-09-18 17:41:09 +0300

    up

---
+++ JS Advanced/02. Arrays and Nested Arrays/1. Print an Array with a Given Delimiter.js
@@ -0,0 +1,12 @@
+function arrayJoin(arr) {
+    const delimeter = arr.pop();
+    const result = arr.join(`${delimeter}`);
+    return result;
+}
+
+console.log(arrayJoin(['One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    '-']));
