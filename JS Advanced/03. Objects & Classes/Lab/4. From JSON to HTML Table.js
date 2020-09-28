86304cb57f29a515caeb4a4c7ba88663981105d9
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-09-28 15:56:08 +0300

    Homework

--- JS Advanced/03. Objects & Classes/Lab/4. From JSON to HTML Table.js
+++ JS Advanced/03. Objects & Classes/Lab/4. From JSON to HTML Table.js
@@ -1,29 +1,18 @@
-function JSONToHTMLTable(jsonString) {
-    let result = '<table>\n';
+function JSONToHTMLTable(input) {
+    let jsonData = input.shift();
+    let students = JSON.parse(jsonData);
 
-    const arr = JSON.parse(jsonString);
-    const titleSet = new Set(arr.map(i => Object.keys(i)).flat());
-    const titleArray = Array.from(titleSet);
+    let first = students[0];
 
-    result += '<tr><th>' + titleArray.join('</th><th>') + '</th></tr>';
+    let html = '<table>';
 
-    result = arr.reduce((acc, currItem) => {
-        let innerResult = titleArray.reduce((titleAcc, currTitle) => {
-            let value = currItem[currTitle];
-            value = value === undefined ? '' : value.toString()
-                .replace(/&/g, "&amp;")
-                .replace(/</g, "&lt;")
-                .replace(/>/g, "&gt;")
-                .replace(/"/g, "&quot;")
-                .replace(/'/g, "&#39;");
+    html += `<tr>${Object.keys(first).map(x => `<th>${x}</th>`).join('')}</tr>`;
+    students.forEach(student => {
+        html += '<tr>';
+        html += Object.values(student).map(x => `<td>${x}</td>`).join('');
+        html += '</tr>';
+    });
+    html += '</table>'
 
-            return titleAcc + '<td>' + value + '</td>';
-        }, '');
-
-        if (innerResult === '') { return acc; }
-        return acc + '\n<tr>' + innerResult + '</tr>';
-    }, result);
-
-    result = result + '\n</table>';
-    return result;
+    console.log(html);
 }
