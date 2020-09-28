8f8e105bc3fdfdd15c6987d097e293a4f0e451ab
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-09-28 16:50:01 +0300

    Homework

---
+++ JS Advanced/03. Objects & Classes/Homework/3. Cappy Juice.js
@@ -0,0 +1,26 @@
+function cappyJuice(input) {
+    let result = {};
+    let juicesInOrder = {};
+
+    input.forEach(line => {
+        let [product, quantity] = line.split(' => ');
+
+        if (!result[product]) {
+            result[product] = Number(quantity);
+        } else {
+            result[product] += Number(quantity);
+        }
+
+        if(result[product] >= 1000){
+            juicesInOrder[product] = 0;
+        }
+    });
+
+    Object.keys(result).forEach(j => {
+        if(juicesInOrder[j] !== undefined) {
+            juicesInOrder[j] += parseInt(result[j] / 1000);
+        }
+    })
+
+    Object.keys(juicesInOrder).forEach(j => console.log(`${j} => ${juicesInOrder[j]}`));
+}
