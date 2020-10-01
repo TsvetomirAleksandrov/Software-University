f49de7689eae0a53f392ed031cc3ca561cb97025
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-01 14:34:46 +0300

    Homework

--- JS Advanced/04. DOM/Lab/02. Format the Text/solution.js
+++ JS Advanced/04. DOM/Lab/02. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO...
+  const allText = document.getElementById('input').textContent;
+
+  let paragraphArr = allText.split('. ');
+  
+  paragraphArr[paragraphArr.length - 1] = paragraphArr[paragraphArr.length - 1].slice(0, -1);
+  paragraphArr = paragraphArr.map((x) => `${x}.`);
+
+  for (let i = 0; i < Math.ceil(paragraphArr.length / 3); i++) {
+    const p = document.createElement('p');
+    p.textContent = paragraphArr.slice(i * 3, i * 3 + 3).join(' ');
+    if(p.textContent.length > 0) {
+      document.getElementById('output').appendChild(p);
+    }
+  }
 }
--- JS Advanced/04. DOM/Lab/04. Visited Websites/solution.js
+++ JS Advanced/04. DOM/Lab/04. Visited Websites/solution.js
@@ -1,4 +1,19 @@
 function solve() {
-
-  //TODO...
+  const links = document.querySelectorAll('a');
+  const visits = document.querySelectorAll('p');
+ 
+ 
+  const updateVisitors = (link, visit) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+ 
+      let count = parseInt(visit.innerHTML.replace(/^\D+/g, ''));
+      visit.innerHTML = `visited ${++count} times`;
+    });
+  };
+ 
+ 
+  for (let i = 0; i < links.length; i++) {
+    updateVisitors(links[i], visits[i]);
+  }
 }
