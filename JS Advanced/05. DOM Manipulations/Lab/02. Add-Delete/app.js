322be7be0e1bddfea43d6b1e2820501db906baa5
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-02 19:32:02 +0300

    Homework

--- JS Advanced/05. DOM Manipulations/Lab/02. Add-Delete/app.js
+++ JS Advanced/05. DOM Manipulations/Lab/02. Add-Delete/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    //TODO...
+    let list = document.getElementById('items');
+    list.addEventListener('click', deleteItem);
+
+    let newLiElement = document.createElement('li');
+    newLiElement.innerHTML = `${document.getElementById('newText').value} <a href = "#">[Delete]</a>`;
+    document.getElementById('items').appendChild(newLiElement);
+    document.getElementById('newText').value = '';
+
+    function deleteItem(e) {
+        e.target.parentNode.remove();
+    }
 }
--- JS Advanced/05. DOM Manipulations/Lab/03. Delete-From-Table/app.js
+++ JS Advanced/05. DOM Manipulations/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let allUsers = document.getElementById('customers').getElementsByTagName('td');
+    let inputUserElement = document.getElementsByTagName('input')[0];
+    let found = false
+
+    for (let user of allUsers) {
+        if (user.textContent === inputUserElement.value) {
+            user.parentNode.remove();
+            inputUserElement.value = '';
+            document.getElementById('result'.textContent = 'Deleted.');
+            found = true;
+        }
+    }
+
+    if(!found) {
+        document.getElementById('result').textContent = 'Not found.'
+    }
 }
