8e0b36cc59b8e0ba8093d4665e25f641253fa41b
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-03 19:57:58 +0300

    Up

--- JS Advanced/05. DOM Manipulations/Lab/06. Highlight-Active/app.js
+++ JS Advanced/05. DOM Manipulations/Lab/06. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focus() {
-    console.log('TODO:...');
+    let parentDiv = document.getElementsByTagName('div')[0];
+    parentDiv.addEventListener('focus', onfocus, true);
+    parentDiv.addEventListener('blur', blur, true);
+
+    function onfocus(e) {
+        e.target.parentNode.classList.add('focused');
+    }
+
+    function blur(e) {
+        e.target.parentNode.classList.remove('focused');
+    }
 }
