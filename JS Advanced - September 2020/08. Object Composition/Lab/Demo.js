e0235f67e04888b90e62c645ca4ca5cf513cd3d7
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-14 13:51:22 +0300

    up

--- JS Advanced - September 2020/08. Object Composition/Lab/Demo.js
+++ JS Advanced - September 2020/08. Object Composition/Lab/Demo.js
@@ -1,14 +1,19 @@
-let cats = [
-    { name: 'Navcho', age: 5 },
-    { name: 'Garry', age: 2 },
-    { name: 'Misho', age: 3 },
+let students = [
+    { name: 'Pesho', score: 1 },
+    { name: 'Gosho', score: 2 },
+    { name: 'Pesho', score: 3 },
+    { name: 'Mariya', score: 2 },
+    { name: 'Ivan', score: 5 },
 ];
 
+let res1 = students.reduce((acc, curr, index, array) => {
+    let same = acc.find(s => s.name === curr.name);
+    if (!same) {
+        acc.push(curr);
+    } else {
+        same.score += curr.score;
+    }
+    return acc;
+}, []);
 
-let newCats = [...cats];
-
-newCats.forEach(x => {
-    x.age++;
-});
-
-console.log(newCats);
+console.log(res1);
