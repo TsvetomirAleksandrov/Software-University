8c36b27dc59b2d0ce2005592dbc16acc243c9ef1
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-18 17:07:27 +0300

    up

---
+++ JS Advanced - September 2020/09. Prototypes and Inheritance/Lab/Demo.js
@@ -0,0 +1,15 @@
+let student2 = {
+    name: 'Petkanncho',
+    hasPen: true,
+    write: function() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+let student1 = {
+    name: 'Ivancho'
+}
+
+Object.setPrototypeOf(student1, student2);
+
+student1.write();
