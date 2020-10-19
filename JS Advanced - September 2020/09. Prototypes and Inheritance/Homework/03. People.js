3582ec8f793ed3463fa7f2c61b018e11e92a53a2
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-19 15:17:16 +0300

    update

--- JS Advanced - September 2020/09. Prototypes and Inheritance/Homework/03. People.js
+++ JS Advanced - September 2020/09. Prototypes and Inheritance/Homework/03. People.js
@@ -1,6 +1,10 @@
 function people() {
     class Employee {
         constructor(name, age) {
+            if (new.target === Employee) {
+                throw new Error('Cannot instantiate directly.');
+            }
+
             this.name = name;
             this.age = age;
             this.salary = 0;
@@ -41,20 +45,20 @@ function people() {
     class Manager extends Employee {
         constructor(name, age) {
             super(name, age);
-            this.dividend = 0;
+            this.divident = 0;
             this.tasks.push('scheduled a meeting.');
             this.tasks.push('is preparing a quarterly report.');
         }
 
         getSalary() {
-            return super.getSalary() + this.dividend;
+            return super.getSalary() + this.divident;
         }
     }
 
     return {
         Employee,
         Junior,
-        Senior, 
+        Senior,
         Manager
     }
 }
