09d92f8f102c790d96796582d52fca3c495db981
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-19 21:51:25 +0300

    up

--- JS Advanced - September 2020/09. Prototypes and Inheritance/Homework/04. Posts.js
+++ JS Advanced - September 2020/09. Prototypes and Inheritance/Homework/04. Posts.js
@@ -1,30 +1,31 @@
 function posts() {
-    class Post{
-        constructor(title, content){
+
+    class Post {
+        constructor(title, content) {
             this.title = title;
             this.content = content;
         }
 
-        toString(){
-            return `Post: ${this.title}\n` + `Content: ${this.content}`;
+        toString() {
+            return `Post: ${this.title}\n ` + `Content: ${this.content}`;
         }
     }
 
-    class SocialMediaPost extends Post{
-        constructor(title, content, likes, dislikes){
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
             super(title, content);
             this.likes = likes;
-            this.dislikes = dislikes;
+            this.dislikes = dislikes;;
             this.comments = [];
         }
 
-        addComment(comment){
-            this.comments.push(comment);
+        addComment(comment) {
+            this.comments.push(this.comment);
         }
 
-        toString(){
-            let string =  super.toString() + `\nRating: ${this.likes - this.dislikes}`;
-            if(this.comments.length > 0){
+        toString() {
+            let string = super.toString() + `\nRating: ${this.likes - this.dislikes}`;
+            if (this.comments.length > 0) {
                 string += "\nComments:";
                 this.comments.forEach(c => string += `\n * ${c}`);
             }
@@ -33,21 +34,25 @@ function posts() {
         }
     }
 
-    class BlogPost extends Post{
-        constructor(title, content, views){
+    class BlogPost extends Post {
+        constructor(title, content, views) {
             super(title, content);
             this.views = views;
         }
 
-        view(){
+        view() {
             this.views++;
             return this;
         }
 
-        toString(){
-            return super.toString() + `\nViews: ${this.views}`
+        toString() {
+            return super.toString() += `\nViews: ${this.views}`
         }
     }
 
-    return {Post, SocialMediaPost, BlogPost}
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost
+    }
 }
