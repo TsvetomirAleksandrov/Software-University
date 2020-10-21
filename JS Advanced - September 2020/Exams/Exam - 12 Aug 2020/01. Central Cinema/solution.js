7d6d7efcc840b8bb47cc2fb3ab3fb303d665ca0a
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-10-21 20:20:00 +0300

    update

--- JS Advanced - September 2020/Exams/Exam - 12 Aug 2020/01. Central Cinema/solution.js
+++ JS Advanced - September 2020/Exams/Exam - 12 Aug 2020/01. Central Cinema/solution.js
@@ -1,95 +1,109 @@
 function solve() {
-    const inputs = document.querySelectorAll('#container > input');
-    const onScreenBtn = document.querySelector('#container > button');
- 
-    const moviesOnScreensUl = document.querySelector('#movies > ul');
-    const archiveSection = document.querySelector('#archive > ul');
- 
-    const clearBtn = document.querySelector('#archive > button');
- 
-    onScreenBtn.addEventListener('click', addMovie);
- 
+    const inputs = Array.from(document.querySelectorAll('#container input'));
+    let [movieName, movieHall, ticketPrice] = inputs;
+    const onScreenButton = document.querySelector('#container button');
+
+    const onScreenMoviesUlElement = document.querySelector('#movies ul');
+    const archiveUlElement = document.querySelector('#archive ul');
+
+    const archiveClearButton = document.querySelector('#archive button');
+
+    onScreenButton.addEventListener('click', addMovie);
+
     function addMovie(e) {
         e.preventDefault();
- 
+
         const movie = {
-            name: inputs[0].value,
-            hall: inputs[1].value,
-            ticketPrice: Number(inputs[2].value)
+            name: movieName.value,
+            hall: movieHall.value,
+            ticketPrice: Number(ticketPrice.value)
         }
- 
-        if (!movie.name || !movie.hall || !movie.ticketPrice) {
+
+        if (!inputs.every(x => x.value)) {
             return;
         }
- 
-        const li = el('li');
- 
-        const movieNameEl = el('span', movie.name);
-        const hallEl = el('strong', `Hall: ${movie.hall}`);
-        const divEl = el('div');
- 
-        const priceEl = el('strong', movie.ticketPrice.toFixed(2));
-        
-        const inputEl = el('input');
-        inputEl.placeholder = "Tickets Sold";
- 
-        const archiveBtn = el('button', 'Archive');
- 
-        divEl.appendChild(priceEl);
-        divEl.appendChild(inputEl);
-        divEl.appendChild(archiveBtn);
- 
-        archiveBtn.addEventListener('click', archiveMovie);
- 
-        li.appendChild(movieNameEl);
-        li.appendChild(hallEl);
-        li.appendChild(divEl);
- 
-        moviesOnScreensUl.appendChild(li);
- 
-        inputs[0].value = '';
-        inputs[1].value = '';
-        inputs[2].value = '';
- 
+
+        if (!Number(ticketPrice.value)) {
+            return;
+        }
+
+        const liElement = el('li');
+
+        const movieNameElement = el('span', movie.name);
+        const hallElement = el('strong', `Hall: ${movie.hall}`);
+        const divElement = el('div');
+
+        const priceElement = el('strong', movie.ticketPrice.toFixed(2));
+
+        const inputElement = el('input');
+        inputElement.setAttribute('placeholder', 'Tickets Sold');
+
+        const archiveButton = el('button', 'Archive');
+
+
+        divElement.appendChild(priceElement);
+        divElement.appendChild(inputElement);
+        divElement.appendChild(archiveButton);
+
+
+        archiveButton.addEventListener('click', archiveMovie);
+
+        liElement.appendChild(movieNameElement);
+        liElement.appendChild(hallElement);
+        liElement.appendChild(divElement);
+
+
+        onScreenMoviesUlElement.appendChild(liElement);
+
+        movieName.value = '';
+        movieHall.value = '';
+        ticketPrice.value = '';
+
         function archiveMovie(e) {
-            let qtty = e.target.previousElementSibling;
-            if (!Number(qtty.value)) {
+            let ticketQuantity = e.target.previousElementSibling;
+
+            if (!Number(ticketQuantity.value)) {
                 return;
             }
- 
-            const liEl = el('li');
-            const movieName = el('span', movie.name);
-            const strongEl = el('strong', `Total amount: ${(Number(qtty.value) * movie.ticketPrice).toFixed(2)}`);
-            const deleteBtn = el('button', 'Delete');
- 
-            liEl.appendChild(movieName);
-            liEl.appendChild(strongEl);
-            liEl.appendChild(deleteBtn);
- 
-            archiveSection.appendChild(liEl);
+
+            const archiveLiElement = el('li');
+
+            const archiveMovieName = el('span', movie.name);
+
+            const archiveTotalAmountElement = el('strong', `Total amount: ${(Number(ticketQuantity.value) * movie.ticketPrice).toFixed(2)}`);
+
+            const archiveDeleteButton = el('button', 'Delete');
+
+            archiveLiElement.appendChild(archiveMovieName);
+            archiveLiElement.appendChild(archiveTotalAmountElement);
+            archiveLiElement.appendChild(archiveDeleteButton);
+
+
+            archiveUlElement.appendChild(archiveLiElement);
             e.target.parentNode.parentNode.remove();
- 
-            deleteBtn.addEventListener('click', function (ev) {
-                ev.target.parentNode.remove();
+
+            archiveDeleteButton.addEventListener('click', e => {
+                e.target.parentNode.remove();
             });
         };
     };
- 
-    clearBtn.addEventListener('click', function (e) {
+
+    archiveClearButton.addEventListener('click', e => {
         e.target.previousElementSibling.textContent = '';
     });
- 
+
+
     function el(type, content, className) {
         let result = document.createElement(type);
- 
+
         if (content) {
             result.textContent = content;
         }
- 
+
         if (className) {
             result.className = className;
         }
- 
+
         return result;
     }
 }
