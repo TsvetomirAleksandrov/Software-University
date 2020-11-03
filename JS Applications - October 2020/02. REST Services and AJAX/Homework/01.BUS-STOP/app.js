6b208e09a53fcdca175ace071a589506747cc49a
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-03 13:24:20 +0200

    REST Services and AJAX

---
+++ JS Applications - October 2020/02. REST Services and AJAX/Homework/01.BUS-STOP/app.js
@@ -0,0 +1,32 @@
+function getInfo() {
+    let stopName = document.getElementById('stopName');
+    let busesUL = document.getElementById('buses');
+    let stopID = document.getElementById('stopId').value;
+
+    fetch(`https://judgetests.firebaseio.com/businfo/${stopID}.json`)
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error('Request error');
+            }
+
+            return response.json();
+        })
+        .then(dataJSON => displayData(dataJSON))
+        .catch(err => displayError(err));
+
+
+    function displayData(dataJSON) {
+        busesUL.textContent = '';
+        stopName.textContent = dataJSON.name;
+        for (const key in dataJSON.buses) {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${key} arrives in ${dataJSON.buses[key]} minutes`;
+            busesUL.appendChild(li);
+        }
+    }
+
+    function displayError(err) {
+        busesUL.innerHTMLM = '';
+        stopName.textContent = 'Error';
+    }
+}
