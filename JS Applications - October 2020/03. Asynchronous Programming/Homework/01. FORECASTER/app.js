a8d9bde199955b19018c7f245eba014332714b9e
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-05 15:17:33 +0200

    Promises added

--- JS Applications - October 2020/03. Asynchronous Programming/Homework/01. FORECASTER/app.js
+++ JS Applications - October 2020/03. Asynchronous Programming/Homework/01. FORECASTER/app.js
@@ -1,5 +1,31 @@
 function attachEvents() {
-    console.log("TODO...");
+
+    let weatherButton = document.getElementById('submit');
+    let locationName = document.getElementById('location');
+
+    const locationsURL = 'https://judgetests.firebaseio.com/locations.json'
+
+
+
+    weatherButton.addEventListener('click', () => {
+        fetch(locationsURL)
+            .then(response => response.json())
+            .then(data => {
+                let { name, code } = data.find(c => c.name === locationName.value);
+
+                let current = fetch(`https://judgetests.firebaseio.com/forecast/today/${code}.json`)
+                    .then(response => response.json());
+
+                let upcoming = fetch(`https://judgetests.firebaseio.com/forecast/upcoming/${code}.json`)
+                    .then(response => response.json());
+
+                Promise.all([current, upcoming])
+                    .then(([currentData, upcomingData]) => {
+                        console.log(currentData);
+                        console.log(upcomingData);
+                    });
+            })
+    })
 }
 
 attachEvents();
