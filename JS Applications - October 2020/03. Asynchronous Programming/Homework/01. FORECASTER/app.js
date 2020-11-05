9d54c3aa6e556df10ded48ef90527bf2f17b2116
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-05 15:53:10 +0200

    Update

--- JS Applications - October 2020/03. Asynchronous Programming/Homework/01. FORECASTER/app.js
+++ JS Applications - October 2020/03. Asynchronous Programming/Homework/01. FORECASTER/app.js
@@ -2,16 +2,25 @@ function attachEvents() {
 
     let weatherButton = document.getElementById('submit');
     let locationName = document.getElementById('location');
+    let currentDiv = document.getElementById('current');
+    let forecastParentDiv = document.getElementById('forecast');
 
     const locationsURL = 'https://judgetests.firebaseio.com/locations.json'
 
+    const symbols = {
+        'Sunny': '&#x2600',           // ☀
+        'Partly sunny': '&#x26C5',    // ⛅
+        'Overcast': '&#x2601',        // ☁
+        'Rain': '&#x2614',            // ☂
+        'degrees': '&#176',           // °
+    };
 
 
     weatherButton.addEventListener('click', () => {
         fetch(locationsURL)
             .then(response => response.json())
             .then(data => {
-                let { name, code } = data.find(c => c.name === locationName.value);
+                let { name, code } = data.find((city) => city.name === locationName.value);
 
                 let current = fetch(`https://judgetests.firebaseio.com/forecast/today/${code}.json`)
                     .then(response => response.json());
@@ -21,11 +30,42 @@ function attachEvents() {
 
                 Promise.all([current, upcoming])
                     .then(([currentData, upcomingData]) => {
-                        console.log(currentData);
-                        console.log(upcomingData);
+
+                        let forecastDiv = createElement('div', 'forecasts', '');
+                        let currentSymbol = symbols[currentData.forecast.condition];
+
+                        let conditionSymbolSpan = createElement('span', 'condition symbol', currentSymbol);
+                        let conditionInfoSpan = createElement('span', 'condition', '');
+
+                        let forecastCitySpan = createElement('span', 'forecast-data', currentData.name);
+
+                        let highLow = `${currentData.forecast.low}${symbols.degrees}/${currentData.forecast.high}${symbols.degrees}`;
+                        let forecastInfoSpan = createElement('span', 'forecast-data', highLow);
+
+                        let forecastConditionSpan = createElement('span', 'forecast-data', currentData.forecast.condition);
+
+
+                        forecastDiv.appendChild(conditionSymbolSpan);
+                        currentDiv.appendChild(forecastDiv);
+                        conditionInfoSpan.appendChild(forecastCitySpan);
+                        conditionInfoSpan.appendChild(forecastInfoSpan);
+                        conditionInfoSpan.appendChild(forecastConditionSpan);
+                        forecastDiv.appendChild(conditionInfoSpan);
+
+                        forecastParentDiv.style.display = 'block';
+
                     });
-            })
+            });
     })
+
+
+    function createElement(ele, classes, content) {
+        let element = document.createElement(ele);
+        element.className = classes;
+        element.innerHTML = content;
+
+        return element;
+    }
 }
 
 attachEvents();
