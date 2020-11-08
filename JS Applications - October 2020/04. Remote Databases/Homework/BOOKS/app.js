bdfcd5282f63e7a6ab42f82dd576128c0a12af75
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-08 14:20:10 +0200

    List all books - Completed

--- JS Applications - October 2020/04. Remote Databases/Homework/BOOKS/app.js
+++ JS Applications - October 2020/04. Remote Databases/Homework/BOOKS/app.js
@@ -4,15 +4,16 @@ const htmlSelectors = {
     'createTitleInput': () => document.getElementById('create-title'),
     'createAuthorInput': () => document.getElementById('create-author'),
     'createIsbnInput': () => document.getElementById('create-isbn'),
-    'booksContainer': () => document.querySelector('tbody')
+    'booksContainer': () => document.querySelector('table > tbody'),
+    'errorContainer': () => document.getElementById('error-notification')
 }
 
 htmlSelectors['loadBooks']()
     .addEventListener('click', fetchAllBooks);
 
 
-function fetchAllBooks(params) {
-    fetch(`https://books-app-8cdaa.firebaseio.com/Books/.json`)
+function fetchAllBooks() {
+    fetch(`https://books-app-8cdaa.firebaseio.com/Book/.json`)
         .then(res => res.json())
         .then(renderBooks)
         .catch(handleError)
@@ -21,32 +22,42 @@ function fetchAllBooks(params) {
 function renderBooks(booksData) {
     const booksContainer = htmlSelectors['booksContainer']();
 
+    if (booksContainer.innerHTML != '') {
+        booksContainer.innerHTML = '';
+    }
+
     Object
         .keys(booksData)
-    foreEach(bookId => {
-        const { title, author, isbn } = booksData[bookId];
+        .forEach(bookId => {
+            const { title, author, isbn } = booksData[bookId];
 
-        const tableRow = createDOMElement('tr', '', {}, {},
-            createDOMElement('td', title, {}, {}),
-            createDOMElement('td', author, {}, {}),
-            createDOMElement('td', isbn, {}, {}),
-            createDOMElement('td', '', {}, {},
-                createDOMElement('button', 'Edit', {}, {}),
-                createDOMElement('button', 'Delete', {}, {})));
+            const tableRow = createDOMElement('tr', '', {}, {},
+                createDOMElement('td', title, {}, {}),
+                createDOMElement('td', author, {}, {}),
+                createDOMElement('td', isbn, {}, {}),
+                createDOMElement('td', '', {}, {},
+                    createDOMElement('button', 'Edit', {}, {}),
+                    createDOMElement('button', 'Delete', {}, {})));
 
-        booksContainer.appendChild(tableRow);
-    })
+            booksContainer.appendChild(tableRow);
+        })
 }
 
 function handleError(err) {
-    console.dir(err);
+    const errorContainer = htmlSelectors['errorContainer']();
+    errorContainer.style.display = 'block';
+    errorContainer.textContent = err.message;
+
+    setTimeout(() => {
+        errorContainer.style.display = 'none';
+    }, 5000);
 }
 
 function createDOMElement(type, content, attributes, events, ...children) {
     const domElement = document.createElement(type);
 
-    if (text !== '') {
-        domElement.textContent = text;
+    if (content !== '') {
+        domElement.textContent = content;
     }
 
     Object.entries(attributes)
@@ -55,12 +66,12 @@ function createDOMElement(type, content, attributes, events, ...children) {
         });
 
     Object.entries(events)
-        .foreEach(([eventName, eventHandler]) => {
+        .forEach(([eventName, eventHandler]) => {
             domElement.addEventListener(eventName, eventHandler);
         });
 
     children
-        .foreEach((child) => {
+        .forEach((child) => {
             domElement.appendChild(child);
         });
 
