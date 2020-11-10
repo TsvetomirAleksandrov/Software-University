b2628a92750776ccc2bc6a9b1c0cf76eee177f43
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-10 21:15:24 +0200

    HTTP Status Cats Exercise Completed

--- JS Applications - October 2020/Templating/Homework/HTTPStatusCats/template.js
+++ JS Applications - October 2020/Templating/Homework/HTTPStatusCats/template.js
@@ -1,8 +1,26 @@
 (() => {
-     renderCatTemplate();
+    let outputSection = document.getElementById('allCats');
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    async function renderCatTemplate() {
+        let html = await (await fetch(`./cat-template.hbs`)).text();
+        let template = Handlebars.compile(html);
+        outputSection.innerHTML = template({ cats });
+    }
+
+    function catStatusBtn() {
+        outputSection.addEventListener('click', (e) => {
+            if (e.target.tagName === 'BUTTON') {
+                if (e.target.parentNode.children[1].style.display === 'none') {
+                    e.target.parentNode.children[1].style.display = 'block';
+                    e.target.textContent = 'Hide status code';
+                } else {
+                    e.target.parentNode.children[1].style.display = 'none';
+                    e.target.textContent = 'Show status code';
+                }
+            }
+        });
+    }
+
+    catStatusBtn();
+    renderCatTemplate();
+})();
