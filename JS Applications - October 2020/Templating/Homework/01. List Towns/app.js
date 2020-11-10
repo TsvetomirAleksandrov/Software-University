8708821bb096f234d8310fb6b7a6a24d23892b09
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-10 20:01:57 +0200

    list-towns updated
    refactored with Partial

--- JS Applications - October 2020/Templating/Homework/01. List Towns/app.js
+++ JS Applications - October 2020/Templating/Homework/01. List Towns/app.js
@@ -1,14 +1,16 @@
 window.addEventListener('load', () => {
+    const templateString = document.getElementById('main-template').innerHTML;
+    Handlebars.registerPartial('town', document.getElementById('town-template').innerHTML);
+    
+    
     document.querySelector('#btnLoadTowns').addEventListener('click', renderTowns);
     const input = document.querySelector('#towns');
     const rootElement = document.querySelector('#root');
 
     function renderTowns(e) {
         e.preventDefault();
-
         const towns = input.value.split(', ');
 
-        const templateString = document.getElementById('main-template').innerHTML;
         const templateFn = Handlebars.compile(templateString);
         const generatedHtml = templateFn({ towns });
 
