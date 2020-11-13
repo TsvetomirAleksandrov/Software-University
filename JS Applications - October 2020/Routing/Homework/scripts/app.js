a30fb8fd1ee8c3733b4fd0da629eb8252cc41560
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-13 22:24:31 +0200

    update

--- JS Applications - October 2020/Routing/Homework/scripts/app.js
+++ JS Applications - October 2020/Routing/Homework/scripts/app.js
@@ -1,47 +1,16 @@
-const router = Sammy('#main', function () {
+import home from '../controllers/home.js';
 
-this.use('Handlebars', 'hbs');
+$(() => {
+    const app = Sammy('#main', function () {
+        this.use('Handlebars', 'hbs');
 
-    this.get('#/home', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs'
-        }).then(function () {
-            this.partial('../templates/home/home.hbs');
-        });
+        this.userData = {
+            loggedIn: false,
+            hasTeam: false
+        };
+
+        this.get('index.html', home);
     });
 
-    this.get('#/about', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
-        }).then(function () {
-            this.partial('../templates/about/about.hbs');
-        });
-    });
-
-    this.get('#/login', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
-            'loginForm': '../templates/login/loginForm.hbs'
-        }).then(function () {
-            this.partial('../templates/login/loginPage.hbs');
-        });
-    });
-
-    this.get('#/register', function () {
-        this.loadPartials({
-            'header': '../templates/common/header.hbs',
-            'footer': '../templates/common/footer.hbs',
-            'loginForm': '../templates/login/loginForm.hbs',
-            'registerForm': '../templates/register/registerForm.hbs',
-        }).then(function () {
-            this.partial('../templates/register/registerPage.hbs');
-        });
-    });
+    app.run();
 });
-
-(() => {
-    router.run('#/home');
-})();
