cca5d58392272b076d4af090434ccc1a7596b636
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-21 20:47:22 +0200

    header partials created

--- JS Applications - October 2020/Exams/ShoeShelf/index.js
+++ JS Applications - October 2020/Exams/ShoeShelf/index.js
@@ -2,27 +2,39 @@ const app = Sammy('#root', function () {
     this.use('Handlebars', 'hbs');
 
 
-    this.get('/home', function () {
-        this.partial('./templates/homeGuest.hbs');
+    this.get('/home', function (context) {
+
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/homeGuest.hbs');
+            })
     });
 
-    this.get('/register', function () {
-        this.partial('./templates/register.hbs');
+    this.get('/register', function (context) {
+
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/register.hbs');
+            })
     });
 
-    this.get('/login', function () {
-        this.partial('./templates/login.hbs');
+    this.get('/login', function (context) {
+        
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/login.hbs');
+            })
     });
 
-    this.get('/create-offer', function () {
+    this.get('/create-offer', function (context) {
         this.partial('./templates/createOffer.hbs');
     });
 
-    this.get('/edit-offer', function () {
+    this.get('/edit-offer', function (context) {
         this.partial('./templates/editOffer.hbs');
     });
 
-    this.get('/details', function () {
+    this.get('/details', function (context) {
         this.partial('./templates/details.hbs');
     });
 });
@@ -30,3 +42,10 @@ const app = Sammy('#root', function () {
 (() => {
     app.run('/home');
 })();
+
+function extendContext(context) {
+    return context.loadPartials({
+        'header': './partials/header.hbs',
+        'footer': './partials/footer.hbs',
+    })
+}
