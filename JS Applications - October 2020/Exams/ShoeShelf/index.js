187944fc9f7a5e33bba99a9f47539e4ad10a5956
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-21 21:16:37 +0200

    register-user functionality added

--- JS Applications - October 2020/Exams/ShoeShelf/index.js
+++ JS Applications - October 2020/Exams/ShoeShelf/index.js
@@ -1,7 +1,9 @@
+const UserModel = firebase.auth();
+
 const app = Sammy('#root', function () {
     this.use('Handlebars', 'hbs');
 
-
+    //Home
     this.get('/home', function (context) {
 
         extendContext(context)
@@ -10,30 +12,52 @@ const app = Sammy('#root', function () {
             })
     });
 
-    this.get('/register', function (context) {
 
+    //Register
+    this.get('/register', function (context) {
         extendContext(context)
             .then(function () {
                 this.partial('./templates/register.hbs');
             })
     });
 
+    this.post('/register', function (context) {
+        const { email, password, rePassword } = context.params;
+
+        if (password !== rePassword) {
+            return;
+        }
+
+        UserModel.createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                this.redirect('/home');
+            })
+            .catch((error) => console.log(error))
+    });
+
+    //Login
     this.get('/login', function (context) {
-        
         extendContext(context)
             .then(function () {
                 this.partial('./templates/login.hbs');
             })
     });
 
+    this.post('/login', function (context) {
+
+    });
+
+    //Create Offer
     this.get('/create-offer', function (context) {
         this.partial('./templates/createOffer.hbs');
     });
 
+    //Edit Offer
     this.get('/edit-offer', function (context) {
         this.partial('./templates/editOffer.hbs');
     });
 
+    //Details
     this.get('/details', function (context) {
         this.partial('./templates/details.hbs');
     });
