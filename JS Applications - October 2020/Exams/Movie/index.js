c4e18f24beecb053357514ddda8c03361b9dfe1a
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-23 20:55:28 +0200

    Movies

---
+++ JS Applications - October 2020/Exams/Movie/index.js
@@ -0,0 +1,101 @@
+const UserModel = firebase.auth();
+const DB = firebase.firestore();
+
+const app = Sammy('#container', function () {
+    this.use('Handlebars', 'hbs');
+
+
+
+    this.get('/home', function (context) {
+        extendContext(context)
+        .then(function () {
+            this.partial('./templates/home.hbs')
+        })
+    })
+
+    this.get('/register', function (context) {
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/register.hbs');
+            })
+    });
+
+    this.post('/register', function (context) {
+        const { email, password, repeatPassword } = context.params;
+
+        if (password !== repeatPassword) {
+            return;
+        }
+
+        UserModel.createUserWithEmailAndPassword(email, password)
+            .then((userData) => {
+                console.log(userData);
+                this.redirect('/login');
+            })
+            .catch(errorHandler);
+    });
+
+    //Login
+    this.get('/login', function (context) {
+        extendContext(context)
+            .then(function () {
+                this.partial('./templates/login.hbs');
+            })
+    });
+
+    this.post('/login', function (context) {
+        const { email, password } = context.params;
+
+        UserModel.signInWithEmailAndPassword(email, password)
+            .then((userData) => {
+                saveUserData(userData)
+                this.redirect('/home');
+            })
+            .catch(errorHandler);
+    });
+
+    //Logout
+    this.get('/logout', function (context) {
+        UserModel.signOut()
+            .then(() => {
+                clearUserData();
+                this.redirect('/home');
+            })
+            .catch(errorHandler);
+    });
+
+
+});
+
+(() => {
+    app.run('/home');
+})();
+
+function extendContext(context) {
+    const user = getUserData();
+    context.isLoggedIn = Boolean(user);
+    context.userEmail = user ? user.email : '';
+
+    return context.loadPartials({
+        'header': './partials/header.hbs',
+        'footer': './partials/footer.hbs',
+    })
+}
+
+function errorHandler(error) {
+    console.log(error);
+}
+
+function saveUserData(data) {
+    const { user: { email, uid } } = data;
+    localStorage.setItem('user', JSON.stringify({ email, uid }));
+}
+
+function getUserData() {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : null;
+}
+
+function clearUserData() {
+    this.localStorage.removeItem('user');
+}
