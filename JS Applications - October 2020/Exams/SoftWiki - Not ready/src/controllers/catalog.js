f0461866a48fd6d1dcb7a9ca49141a4705154667
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-29 16:51:41 +0200

    update

--- JS Applications - October 2020/Exams/SoftWiki - Not ready/src/controllers/catalog.js
+++ JS Applications - October 2020/Exams/SoftWiki - Not ready/src/controllers/catalog.js
@@ -4,10 +4,34 @@ import { getAll } from '../data.js';
 
 export async function homePage() {
     await addPartials(this);
+    this.partials.article = await this.load('/templates/catalog/article.hbs');
 
-    const data = await getAll();
+    // const context = {
+    //     articles: await getAll()
+    // };
 
-    
+    const context = {
+        js: [
+            {
+                title: 'Article1',
+                category: 'JavaScript',
+                content: 'Lorem ipsum dolor sit amet'
+            },
+            {
+                title: 'Article2',
+                category: 'JavaScript',
+                content: 'dolor sit amet'
+            }
+        ],
+        java: [
+            {
+                title: 'Article1',
+                category: 'Java',
+                content: 'Lorem ipsum dolor sit amet'
+            },
+        ],
+    };
 
-    this.partial('/templates/catalog/homePage.hbs');
+
+    this.partial('/templates/catalog/homePage.hbs', context);
 }
