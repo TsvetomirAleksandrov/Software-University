0ffab9b13894656cfa7ed262fce8dd09e39f84be
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-11-30 18:06:50 +0200

    MyBlog - Fully Working SPA
    Notifications can be implemented.

--- JS Applications - October 2020/Exams/MyBlog - Not ready/app.js
+++ JS Applications - October 2020/Exams/MyBlog - Not ready/app.js
@@ -10,7 +10,20 @@ const app = Sammy('#root', function () {
         DB.collection('posts')
             .get()
             .then((response) => {
-                context.post = response.docs.map((post) => { return { id: post.id, ...post.data() } });
+
+                const user = getUserData();
+
+                context.post = response.docs.map((post) => {
+                    if (user) {
+                        return {
+                            id: post.id,
+                            ...post.data(),
+                            isCreator: post.data().creator === user.email
+                        }
+                    }
+                });
+
+                console.log(context.post);
                 extendContext(context)
                     .then(function () {
                         this.partial('./templates/home.hbs')
@@ -87,16 +100,8 @@ const app = Sammy('#root', function () {
             .doc(postId)
             .get()
             .then((response) => {
-                const { uid, email } = getUserData();
-                const actualPostData = response.data();
 
-                const isCreator = actualPostData.creator === email;
-
-                //const userIndex = actualPostData.likes.indexOf(uid);
-                //const iLiked = userIndex > -1;
-                //const likesCount = actualPostData.likes.length;
-
-                context.post = { ...response.data(), isCreator, id: postId };
+                context.post = { ...response.data(), id: postId };
                 extendContext(context)
                     .then(function () {
                         this.partial('./templates/details.hbs')
@@ -104,23 +109,14 @@ const app = Sammy('#root', function () {
             })
     });
 
-    //Create Post
-    this.get('/create-post', function (context) {
-        extendContext(context)
-            .then(function () {
-                this.partial('./templates/createPost.hbs')
-            })
-    });
-
     this.post('/create-post', function (context) {
         const { title, category, content } = context.params;
 
         DB.collection('posts').add({
-            title, 
-            category, 
+            title,
+            category,
             content,
-            creator: getUserData().email,
-            
+            creator: getUserData().email
         })
             .then((data) => {
                 this.redirect('/home');
@@ -145,7 +141,7 @@ const app = Sammy('#root', function () {
     });
 
     this.post('/edit/:postId', function (context) {
-        const { title, category, content } = context.params;
+        const { postId, title, category, content } = context.params;
 
         DB.collection('posts')
             .doc(postId)
@@ -178,28 +174,6 @@ const app = Sammy('#root', function () {
             })
             .catch(errorHandler);
     });
-
-    // //Like recipe
-    // this.get('/like/:recipeId', function (context) {
-    //     const { recipeId } = context.params;
-    //     const { uid } = getUserData();
-
-    //     DB.collection('recipes')
-    //         .doc(recipeId)
-    //         .get()
-    //         .then((response) => {
-    //             const recipeData = { ...response.data() };
-    //             recipeData.likes.push(uid);
-
-    //             return DB.collection('recipes')
-    //                 .doc(recipeId)
-    //                 .set(recipeData)
-    //         })
-    //         .then(() => {
-    //             this.redirect(`#/details/${recipeId}`);
-    //         })
-    //         .catch(errorHandler);
-    // })
 });
 
 (() => {
@@ -218,11 +192,11 @@ function extendContext(context) {
 
 function saveUserData(data) {
     const { user: { email, uid } } = data;
-    sessionStorage.setItem('user', JSON.stringify({ email, uid }));
+    window.sessionStorage.setItem('user', JSON.stringify({ email, uid }));
 }
 
 function getUserData() {
-    const user = sessionStorage.getItem('user');
+    const user = window.sessionStorage.getItem('user');
     return user ? JSON.parse(user) : null;
 }
 
@@ -233,56 +207,3 @@ function clearUserData() {
 function errorHandler(error) {
     console.log(error);
 }
-
-// function errorHandler(msg) {
-//     let errorBox = document.getElementById('errorBox');
-
-//     errorBox.textContent = msg;
-//     errorBox.style.display = 'block';
-
-//     setTimeout(() => errorBox.style.display = 'none', 3000);
-// }
-
-// function successHandler(msg) {
-//     let successBox = document.getElementById('successBox');
-
-//     successBox.textContent = msg;
-//     successBox.style.display = 'block';
-
-//     setTimeout(() => successBox.style.display = 'none', 3000);
-// }
-
-// function loadingHandler(msg) {
-//     let loadingBox = document.getElementById('loadingBox');
-
-//     loadingBox.textContent = msg;
-//     loadingBox.style.display = 'block';
-
-//     setTimeout(() => loadingBox.style.display = 'none', 3000);
-// }
-
-// function categoryImage(category) {
-//     let categoryImageURL = '';
-
-//     if (category === 'Vegetables and legumes/beans') {
-//         categoryImageURL = 'https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549__340.jpg';
-//     }
-
-//     if (category === 'Fruits') {
-//         categoryImageURL = 'https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029__340.jpg';
-//     }
-
-//     if (category === 'Milk, cheese, eggs and alternatives') {
-//         categoryImageURL = 'https://image.shutterstock.com/image-photo/assorted-dairy-products-milk-yogurt-260nw-530162824.jpg';
-//     }
-
-//     if (category === 'Grain Food') {
-//         categoryImageURL = 'https://www.newskarnataka.com/storage/photos/shares/FCI_M_2852020.jpg';
-//     }
-
-//     if (category === 'Lean meats and poultry, fish and alternatives') {
-//         categoryImageURL = 'https://t3.ftcdn.net/jpg/01/18/84/52/240_F_118845283_n9uWnb81tg8cG7Rf9y3McWT1DT1ZKTDx.jpg';
-//     }
-
-//     return categoryImageURL;
-// }
