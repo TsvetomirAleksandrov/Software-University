08099077c4f9b9756c6d88692178f7d6f35cd9a7
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 15:45:46 +0200

    Notification

--- Motion Software Bootcamp/DOM Manipulations/Exercises/05. Notification/scripts.js
+++ Motion Software Bootcamp/DOM Manipulations/Exercises/05. Notification/scripts.js
@@ -1,3 +1,13 @@
 function notify(message) {
-    console.log('TODO:...');
+    let divElement = document.getElementById('notification');
+    show(divElement, message);
+    hide(divElement);
+
+    function show(element, msg) {
+        element.textContent = `${msg}`;
+        element.style.display = 'block';
+    }
+    function hide(element) {
+        setTimeout(function () { element.style.display = 'none'; }, 2000);
+    }
 }
