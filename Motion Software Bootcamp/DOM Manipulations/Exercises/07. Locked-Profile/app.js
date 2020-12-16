279d151e5724fba92faf1656e0f8667c23d237fc
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 16:02:43 +0200

    Locked Profile

--- Motion Software Bootcamp/DOM Manipulations/Exercises/07. Locked-Profile/app.js
+++ Motion Software Bootcamp/DOM Manipulations/Exercises/07. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profiles = document.getElementsByClassName('profile');
+    for (const profile of Array.from(profiles)) {
+        profile.addEventListener('click', showMore);
+    }
+
+    function showMore(e) {
+        let unlocked = e.target.parentNode.getElementsByTagName('input')[1].checked;
+
+        if (e.target.tagName.toLowerCase() === 'button' && unlocked === true) {
+            if (e.target.textContent === 'Show more') {
+                e.target.parentNode.getElementsByTagName('div')[0].style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                e.target.parentNode.getElementsByTagName('div')[0].style.display = 'none';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
