683194dca4aac453eb44bbf8536ee403ab5f876b
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 14:06:17 +0200

    Subtraction

--- Motion Software Bootcamp/DOM Manipulations/Exercises/01. Subtraction/subtract.js
+++ Motion Software Bootcamp/DOM Manipulations/Exercises/01. Subtraction/subtract.js
@@ -1,3 +1,12 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumber = document.getElementById('firstNumber');
+    let secondNumber = document.getElementById('secondNumber');
+    let resultDiv = document.getElementById('result');
+
+    let sub = Number(firstNumber.value) - Number(secondNumber.value)
+
+    if (firstNumber.value !== '' && secondNumber.value !== '') {
+        resultDiv.textContent = `${sub}`;
+    }
+
 }
