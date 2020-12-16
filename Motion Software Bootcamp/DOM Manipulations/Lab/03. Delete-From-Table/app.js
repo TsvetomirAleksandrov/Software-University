a25169ca8f15b71fe790e35021634f8ac982372a
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 11:33:09 +0200

    Delete from Table

--- Motion Software Bootcamp/DOM Manipulations/Lab/03. Delete-From-Table/app.js
+++ Motion Software Bootcamp/DOM Manipulations/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let allUsers = document.getElementById('customers').getElementsByTagName('td');
+    let inputUserElement = document.querySelector('input[type=text]');
+    let found = false;
+
+    for (user of allUsers) {
+        if (inputUserElement.value === user.textContent) {
+            deleteUser(user);
+            inputUserElement.value = '';
+            document.getElementById('result').textContent = 'Deleted.';
+            found = true;
+        }
+    }
+
+    if (!found) {
+        document.getElementById('result').textContent = 'Not found.';
+    }
+
+    function deleteUser(user) {
+        user.parentNode.remove();
+    }
 }
