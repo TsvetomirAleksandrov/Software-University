e726edb3c1801531f8ba27ed43df522473f7ccbd
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 14:45:52 +0200

    Accordion

--- Motion Software Bootcamp/DOM Manipulations/Exercises/03. Accordion/accordion.js
+++ Motion Software Bootcamp/DOM Manipulations/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,7 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.getElementsByClassName('button')[0];
+    let extraText = document.getElementById('extra');
+
+    buttonElement.textContent = buttonElement.textContent === 'More' ? 'Less' : 'More';
+    extraText.style.display = extraText.style.display === 'none' || !extraText.style.display ? 'block' : 'none';
 }
