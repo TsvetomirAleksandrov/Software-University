9f4f1133a31d1faa0afcea442db1b3a4522ebe6e
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 11:23:46 +0200

    update

--- Motion Software Bootcamp/DOM Manipulations/Lab/01. List-Of-Items/app.js
+++ Motion Software Bootcamp/DOM Manipulations/Lab/01. List-Of-Items/app.js
@@ -1,18 +1,17 @@
+let ulElement = document.getElementById('items');
+let input = document.querySelectorAll('input')[0];
+let addBtn = document.querySelectorAll('input')[1];
+
+addBtn.addEventListener('click', addItem);
+
 function addItem() {
-    let ulElement = document.getElementById('items');
-    let input = document.querySelectorAll('input')[0];
-    let addBtn = document.querySelectorAll('input')[1];
+    let liElement = document.createElement('li');
 
-    addBtn.addEventListener('click', addItem);
-
-    function addItem() {
-        let liElement = document.createElement('li');
-
-        if (input.value !== '') {
-            liElement.innerHTML = input.value;
-            ulElement.appendChild(liElement);
-        }
-
-        input.value = '';
+    if (input.value !== '') {
+        liElement.innerHTML = input.value;
+        ulElement.appendChild(liElement);
     }
+
+    input.value = '';
 }
+
--- Motion Software Bootcamp/DOM Manipulations/Lab/02. Add-Delete/app.js
+++ Motion Software Bootcamp/DOM Manipulations/Lab/02. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    let ulElement = document.getElementById('items');
+    let input = document.getElementById('newText');
+    let liElement = document.createElement('li');
+
+    if (input.value !== '') {
+        liElement.innerHTML = `${input.value} <a href='#'>[Delete]</a>`;
+        ulElement.appendChild(liElement);
+        ulElement.addEventListener('click', deleteItem);
+    }
+
+    input.value = '';
+
+    function deleteItem(e) {
+        e.target.parentNode.remove();
+    }
 }
