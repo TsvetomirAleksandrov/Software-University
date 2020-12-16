9972970d60db57e63fcbf82348ce722d9d22458e
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 10:45:47 +0200

    list of items

--- Motion Software Bootcamp/DOM Manipulations/Lab/01. List-Of-Items/app.js
+++ Motion Software Bootcamp/DOM Manipulations/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    let ulElement = document.getElementById('items');
+    let input = document.querySelectorAll('input')[0];
+    let addBtn = document.querySelectorAll('input')[1];
+
+    addBtn.addEventListener('click', addItem);
+
+    function addItem() {
+        let liElement = document.createElement('li');
+
+        if (input.value !== '') {
+            liElement.innerHTML = input.value;
+            ulElement.appendChild(liElement);
+        }
+
+        input.value = '';
+    }
 }
