591683e5c55a32bd28635619cb958c2c5b9c2a49
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-16 13:35:59 +0200

    Stopwatch

--- Motion Software Bootcamp/DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
+++ Motion Software Bootcamp/DOM Manipulations/Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,42 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let startButton = document.getElementById('startBtn');
+    let stopButton = document.getElementById('stopBtn');
+    let time = document.getElementById('time');
+    let timer = 0;
+
+    startButton.addEventListener('click', () => {
+        time.textContent = '00:00';
+        timer = setInterval(addSec, 1000);
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    })
+
+    stopButton.addEventListener('click', () => {
+        clearInterval(timer);
+        startButton.disabled = false;
+        stopButton.disabled = true;
+    })
+
+    let seconds = 0;
+    let minutes = 0;
+
+    function addSec() {
+        seconds++;
+        if (seconds < 10) {
+            seconds = `0` + seconds;
+        }
+        if (seconds >= 60) {
+            seconds = `0` + 0;
+            minutes++;
+            if (minutes < 10) {
+                minutes = `0` + minutes;
+            }
+        }
+        if (minutes === 0) {
+            minutes = `0` + minutes;
+        }
+        time.textContent = `${minutes}:${seconds}`;
+    }
+
+
 }
