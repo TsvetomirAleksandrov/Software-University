21108c1046abe5b4d2e56051d73951b3d86d3a4f
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-21 22:12:45 +0200

    Bus-Stop

--- Motion Software Bootcamp/REST Services & AJAX/Exercises/01.BUS-STOP/app.js
+++ Motion Software Bootcamp/REST Services & AJAX/Exercises/01.BUS-STOP/app.js
@@ -1,3 +1,34 @@
 function getInfo() {
-    
+    let stopName = document.getElementById('stopName');
+    let busesUl = document.getElementById('buses');
+    let stopID = document.getElementById('stopId').value;
+
+    fetch(`https://judgetests.firebaseio.com/businfo/${stopID}.json`)
+        .then(response => {
+            if (response.status !== 200) {
+                throw new Error(`Bad Request!`);
+            }
+            return response.json();
+        })
+        .then(jsonData => displayData(jsonData))
+        .catch(err => displayError(err))
+
+    function displayError(err) {
+        busesUl.textContent = '';
+        stopName.textContent = 'Error!'
+    }
+
+    function displayData(jsonData) {
+        busesUl.textContent = '';
+        stopName.textContent = jsonData.name;
+
+        for (const bus in jsonData.buses) {
+            let liElement = document.createElement('li');
+            liElement.textContent = `Bus ${bus} arrives in ${jsonData.buses[bus]} minutes`;
+            busesUl.appendChild(liElement);
+        }
+    }
 }
+
+
+
