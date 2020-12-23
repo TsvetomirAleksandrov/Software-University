b3320a9a7e943426e38e72203f27a9a9c2a31160
alexandrovbg <cecko.aleksandrov@gmail.com>
2020-12-23 19:40:16 +0200

    Messenger

--- Motion Software Bootcamp/REST Services & AJAX/Exercises/04.MESSENGER/app.js
+++ Motion Software Bootcamp/REST Services & AJAX/Exercises/04.MESSENGER/app.js
@@ -1,5 +1,58 @@
 function attachEvents() {
-    console.log('TODO...');
+    let textArea = document.getElementById('messages');
+    let btnSend = document.getElementById('submit');
+    let btnRefresh = document.getElementById('refresh');
+    let url = `https://rest-messanger.firebaseio.com/messanger.json`;
+
+    btnRefresh.addEventListener('click', function () {
+        textArea.value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('content').value = '';
+        sendRequest(url, 'GET');
+    })
+
+    btnSend.addEventListener('click', function () {
+        let data = {
+            author: document.getElementById('author').value,
+            content: document.getElementById('content').value
+        }
+
+        if (data.author.length > 0 && data.content.length > 0) {
+            document.getElementById('author').value = '';
+            document.getElementById('content').value = '';
+            sendRequest(url, 'POST', data);
+        }
+    });
+
+    function sendRequest(url, method, data) {
+        let obj = {
+            method,
+            body: JSON.stringify(data)
+        }
+
+        if (data === undefined) {
+            delete obj.body;
+        }
+
+        fetch(url, obj)
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('Something went wrong!');
+                }
+
+                return response.json();
+            })
+            .then(data => dataHandler(data, method))
+            .catch(err => textArea.value = err);
+    }
+
+    function dataHandler(data, method) {
+        if (method === 'GET') {
+            for (const key in data) {
+                textArea.value += `${data[key].author}: ${data[key].content}\n`;
+            }
+        }
+    }
 }
 
 attachEvents();
