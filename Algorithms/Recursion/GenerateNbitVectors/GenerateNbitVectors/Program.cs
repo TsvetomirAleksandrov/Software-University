82a111c352a8e07cb1f3181a1f7e1a03e7c093a0
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-06 23:07:52 +0200

    GenerateNbitVectors

---
+++ Algorithms/Recursion/GenerateNbitVectors/GenerateNbitVectors/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenerateNbitVectors
+{
+    class Program
+    {
+static void Generate(int index, int[] vector)
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(' ', vector));
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    vector[index] = i;
+                    Generate(index + 1, vector);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var arr = new int[8];
+            Generate(0, arr);
+        }
+    }
+}
