b943d6b2f04f6979e19037b0ee3deed7d58059be
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-03-19 20:24:05 +0200

    BinarySearch

---
+++ Algorithms/Sorting and Searching/BinarySearch/BinarySearch/BinarySearch/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int IndexOf(int[] arr, int key)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (key < arr[mid])
+                {
+                    high = mid - 1;
+                }
+                else if (key > arr[mid])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
