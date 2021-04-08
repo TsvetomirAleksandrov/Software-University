e00d22d3908e5474332a099c52fee644319c461d
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-08 10:24:39 +0300

    extract express config

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/config/express.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/config/express.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const handlebars = require('handlebars');
+const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-module.exports = (app) => {
+function setupExpress(app) {
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
 
-    // TODO: Setup the view engine
+    app.set('view engine', 'hbs');
 
-    // TODO: Setup the body parser 
-
-    // TODO: Setup the static files
+    app.use(express.static('public'));
 }
+
+module.exports = setupExpress;
