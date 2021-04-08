0bf0b63324d0e22bcba542a659bea81b26910d4a
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-08 17:38:53 +0300

    add basic model architecture

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/productService.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/productService.js
@@ -3,7 +3,7 @@ const Cube = require('../models/Cube');
 const productData = require('../data/productData');
 
 function getAll(query) {
-    let products = productData.getAll();
+    let products = Cube.getAll();
 
     if (query.search) {
         products = products.filter(x => x.name.toLowerCase().includes(query.search));
@@ -21,7 +21,7 @@ function getAll(query) {
 }
 
 function getOne(id) {
-    return productData.getOne(id);
+    return Cube.getOne(id);
 }
 
 function create(data) {
@@ -33,7 +33,8 @@ function create(data) {
         data.difficultyLevel
     );
 
-    return productData.create(cube);
+    // return productData.create(cube);
+    return cube.save();
 }
 
 module.exports = {
