6a715599206b63b322e8624edb5516ec5effb72e
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-08 11:55:01 +0300

    Parse form data

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/controllers/productController.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/controllers/productController.js
@@ -2,11 +2,19 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Browse'});
+    res.render('home', { title: 'Browse' });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create'});
+    res.render('create', { title: 'Create' });
+});
+
+router.post('/create', (req, res) => {
+    res.send('created')
+});
+
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Product Details' });
 });
 
 module.exports = router;
