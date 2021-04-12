c9c8dca1ca805aa77a96f562a720730cd79a5cb7
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-12 21:32:39 +0300

    auth service

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/authService.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/authService.js
@@ -3,12 +3,29 @@ const { SALT_ROUNDS } = require('../config');
 const User = require('../models/User');
 
 const register = async ({ username, password }) => {
+    //TODO: Check if username exists
+
     let salt = await bcrypt.genSalt(SALT_ROUNDS);
-    let hash = bcrypt.hash(password, salt);
+    let hash = await bcrypt.hash(password, salt);
 
     const user = new User({ username, password: hash });
+
+    return await user.save();
+};
+
+const login = async ({ username, password }) => {
+    //get user from db
+    let user = await User.findOne({ username });
+
+    if (!user) throw { message: 'User not found...' };
+    
+
+    //compare password hash
+
+    //generate token
 };
 
 module.exports = {
     register,
+    login,
 }
