5196d18d902a468fa3c466426d27f22ddfe328b7
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-13 12:22:17 +0300

    auth controller

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/controllers/authController.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/controllers/authController.js
@@ -1,28 +1,33 @@
 const { Router } = require('express');
 const authService = require('../services/authService');
-
 const router = Router();
 
-router.get('/login', (req, res) => {
+const isAuthenticated = require('../middlewares/isAuthenticated');
+const isGuest = require('../middlewares/isGuest');
+
+const { COOKIE_NAME } = require('../config');
+
+router.get('/login', isGuest, (req, res) => {
     res.render('login');
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
     const { username, password } = req.body;
 
     try {
         let token = await authService.login({ username, password });
-        res.end();
+        res.cookie(COOKIE_NAME, token);
+        res.redirect('/products');
     } catch (error) {
         res.render('login', { error });
     }
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest, (req, res) => {
     res.render('register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', isGuest, async (req, res) => {
     const { password, repeatPassword, username } = req.body;
 
     if (password !== repeatPassword) {
@@ -31,11 +36,16 @@ router.post('/register', async (req, res) => {
     }
 
     try {
-        let result = await authService.register({ username, password });
-        res.redirect('/products');
+        let user = await authService.register({ username, password });
+        res.redirect('/auth/login');
     } catch (error) {
         res.render('register', { error });
     }
+});
+
+router.get('/logout', isAuthenticated, (req, res) => {
+    res.clearCookie(COOKIE_NAME);
+    res.redirect('/products');
 });
 
 module.exports = router;
