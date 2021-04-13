b927fe075d01783284732a22bfb6a516b93aa21a
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-04-13 12:21:01 +0300

    product service

--- JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/productService.js
+++ JS Back End - January 2021/4. Workshop ExpressJS and Templating/Cubicle/services/productService.js
@@ -3,15 +3,15 @@ const Cube = require('../models/Cube');
 
 async function getAll(query) {
     let products = await Cube.find({}).lean();
-
+    
     if (query.search) {
         products = products.filter(x => x.name.toLowerCase().includes(query.search));
     }
-
+    
     if (query.from) {
         products = products.filter(x => Number(x.level) >= query.from);
     }
-
+    
     if (query.to) {
         products = products.filter(x => Number(x.level) <= query.to);
     }
@@ -19,7 +19,7 @@ async function getAll(query) {
     return products;
 }
 
-async function getOne(id) {
+function getOne(id) {
     return Cube.findById(id).lean();
 }
 
@@ -29,24 +29,34 @@ function getOneWithAccessories(id) {
         .lean();
 }
 
-function create(data) {
-    let cube = new Cube(data);
+function create(data, userId) {
+    let cube = new Cube({...data, creator: userId});
 
     return cube.save();
 }
 
 async function attachAccessory(productId, accessoryId) {
-    let product = await Cube.findById(productId);
-    let accessory = await Accessory.findById(accessoryId);
+    let product = await Cube.findById(productId)
+    let accessry = await Accessory.findById(accessoryId);
 
-    product.accessories.push(accessory);
+    product.accessories.push(accessry);
     return product.save();
 }
 
+function updateOne(productId, productData) {
+    return Cube.updateOne({_id: productId}, productData);
+}
+
+function deleteOne(productId) {
+    return Cube.deleteOne({_id: productId});
+}
+
 module.exports = {
+    getAll,
     getOne,
     getOneWithAccessories,
-    getAll,
     create,
     attachAccessory,
+    updateOne,
+    deleteOne,
 }
