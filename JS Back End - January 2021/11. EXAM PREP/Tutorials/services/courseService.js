753eb3a7fc9417ec195ffcb82cf49c19f00b90ba
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 18:21:15 +0300

    Create courseService.js

---
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/services/courseService.js
@@ -0,0 +1,58 @@
+const Course = require('../models/Course');
+const User = require('../models/User');
+
+async function create(data, userId) {
+    let course = new Course({ ...data, creator: userId })
+    return await course.save();
+}
+
+async function getAll(search) {
+    let result;
+    if (search) {
+        result = await Course.find({ title: { $regex: search, $options: 'i' } }).sort({ createdAt: -1 }).lean();
+    } else {
+        result = await Course.find().sort({ createdAt: -1 }).lean();
+    }
+
+    return result;
+}
+
+async function getSpecific(id, userId) {
+    let course = await Course.findById(id).populate('enrolledUsers').lean();
+    course.isEnrolled = course.enrolledUsers.some(x => x._id == userId);
+    course.isCreator = course.creator == userId;
+    return course;
+}
+
+async function update(id, data) {
+    return await Course.updateOne({ _id: id }, data);
+}
+
+async function deleteCourse(id) {
+    return await Course.deleteOne({ _id: id });
+}
+
+
+async function enroll(courseId, userId) {
+    let course = await Course.findById(courseId);
+    if (!course.enrolledUsers.includes(userId)) {
+        course.enrolledUsers.push(userId);
+    }
+
+    await course.save();
+}
+
+async function getMostPop(num) {
+    return await Course.find({ isPublic: true }).sort({ enrolledUsers: -1 }).limit(num).lean();
+}
+
+
+module.exports = {
+    create,
+    getAll,
+    getSpecific,
+    update,
+    deleteCourse,
+    enroll,
+    getMostPop
+}
