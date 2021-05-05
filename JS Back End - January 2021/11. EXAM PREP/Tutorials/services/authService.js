42b22e450a762e491102f3de2539f0a9b32985af
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 11:15:08 +0300

    Update authService

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/services/authService.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/services/authService.js
@@ -10,14 +10,14 @@ const register = (username, password) => {
 }
 
 const login = async (username, password) => {
-    let user = await User.findOne({ username });
-    if (!user) return Promise.reject({ message: 'No such user', status: 404 });
+    const user = await User.findOne({ username });
+    if (!user) throw { message: 'No such user', status: 404 };
 
-    let areEqual = await bcrypt.compare(password, user.password)
-    if (!areEqual) return Promise.reject({ message: 'Invalid password', status: 404 });
+    const areEqual = await bcrypt.compare(password, user.password);
+    if (!areEqual) throw { message: 'Invalid password', status: 404 };
 
-    let token = jwt.sign({ _id: user._id, username: user.username }, SECRET);
-    return user;
+    const token = jwt.sign({ _id: user._id, username: user.username }, SECRET);
+    return token;
 }
 
 module.exports = {
