a03ca98be56372a12fd3c974a3eb5b467cc8300c
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 18:21:34 +0300

    Update routes.js

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/routes.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/routes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
-const authController = require('./controllers/courseController');
+const courseController = require('./controllers/courseController');
 
 router.use('/', homeController);
 router.use('/auth', authController);
-router.use('/create', courseController);
+router.use('/course', courseController);
 
 module.exports = router;
