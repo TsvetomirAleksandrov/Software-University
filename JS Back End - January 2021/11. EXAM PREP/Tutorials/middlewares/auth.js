e25eaa6803bbdf77ce025130683470be61f2992d
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 18:22:41 +0300

    Update auth middleware

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/middlewares/auth.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/middlewares/auth.js
@@ -1,22 +1,19 @@
-const { COOKIE_NAME, SECRET } = require('../config/config');
+const { SECRET, COOKIE_NAME } = require('../config/config');
 const jwt = require('jsonwebtoken');
 
-function auth(req, res, next) {
+module.exports = function auth(req, res, next) {
     let token = req.cookies[COOKIE_NAME];
-
     if (token) {
         jwt.verify(token, SECRET, function (err, decoded) {
             if (err) {
                 res.clearCookie(COOKIE_NAME);
             } else {
                 req.user = decoded;
-                req.locals.user = decoded;
-                req.locals.isAuth = true;
+                res.locals.user = decoded;
+                res.locals.isAuth = true;
             }
         });
-    }
 
+    }
     next();
 }
-
-module.exports = auth;
