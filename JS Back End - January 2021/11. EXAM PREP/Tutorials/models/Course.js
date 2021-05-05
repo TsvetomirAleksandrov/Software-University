9239cbe4a86a7a8efdd76d00c0db5e19bdfcf331
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 18:22:01 +0300

    Update Course.js

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/models/Course.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/models/Course.js
@@ -1,32 +1,50 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
+    id: mongoose.Types.ObjectId,
     title: {
         type: String,
         required: true,
         unique: true,
+        minlength: [4, 'Title should be at least 4 characters long']
     },
     description: {
         type: String,
-        required: true,
-        maxlength: 50,
+        required: ['Description is required'],
+        minlength: [20, 'Description should be at least 20 characters long'],
+        maxlength: [50, 'Description should be max 50 characters long']
     },
     imageUrl: {
         type: String,
-        required: true,
+        required: ['Image is required'],
+        validate: {
+            validator: (v) => {
+                return /^https?:\/\//.test(v)
+            },
+            message: (props) => {
+                return `The Image URL should start with http:// or https://`
+            }
+        }
     },
     isPublic: {
         type: Boolean,
-        default: false,
+        default: false
     },
     createdAt: {
         type: Date,
         required: true,
     },
-    usersEnrolled: [{
+    enrolledUsers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    creator: {
         type: mongoose.Types.ObjectId,
         ref: 'User'
-    }],
+    }
 });
+
 
 module.exports = mongoose.model('Course', courseSchema);
