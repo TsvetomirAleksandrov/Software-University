281225b441e434008b878e6001679dd1513c7f8f
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 11:15:47 +0300

    Update DB__URI in config

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/config/config.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     PORT: 4000,
-    DB_URI: `mongodb://localhost/tut`,
+    DB_URI: `mongodb://localhost/course-tutorials`,
     SALT_ROUNDS: 10,
     SECRET: 'MNOGOQKASOL',
     COOKIE_NAME: 'TOKEN'
