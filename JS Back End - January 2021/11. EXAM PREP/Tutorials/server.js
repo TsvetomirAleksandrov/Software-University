236aebfd827b4f46af77bf5e0ca2944f0e09a9cb
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-05-05 18:21:28 +0300

    Update server.js

--- JS Back End - January 2021/11. EXAM PREP/Tutorials/server.js
+++ JS Back End - January 2021/11. EXAM PREP/Tutorials/server.js
@@ -1,29 +1,11 @@
-const express = require('express');
+const router = require('express').Router();
+const mainController = require('./controllers/mainController');
+const authController = require('./controllers/authController');
+const courseController = require('./controllers/courseController')
+const isAuth = require('./middlewares/isAuth');
 
-const routes = require('./routes');
-const errorHandler = require('./middlewares/errorHandler');
-const { PORT } = require('./config/config');
+router.use('/', mainController);
+router.use('/auth', authController);
+router.use('/course', isAuth, courseController)
 
-const app = express();
-
-require('./config/mongoose');
-require('./config/express')(app);
-
-app.use(routes);
-app.use(errorHandler);
-
-// TODO: Express config - OK
-// TODO: Add routes and controllers - OK
-// TODO: Add services - OK
-// TODO: Add error handler - OK
-// TODO: Sample view and layout - OK
-// TODO: Add user model
-// TODO: Hash Password
-// TODO: User register
-// TODO: User login
-// TODO: User logout
-// TODO: Authentication middleware
-// TODO: Authorization middleware
-// TODO: Notifications
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+module.exports = router;
